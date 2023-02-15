8f12223518fb9b3a4998932112c1b31582411425
Cristian Mello <cristianc.mello@gmail.com>
2023-02-15 08:31:06 -0300

    First commit

---
+++ src/test/java/com/example/katabddkafka/KataBddKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katabddkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataBddKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
